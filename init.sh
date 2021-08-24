de5483573cbc45e869cbe6a535df6ee7f561c013
vincy <vincywindy@live.cn>
2021-08-24 21:05:18 +0800

    跳过更新

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 
 # 运行 xvfb
 Xvfb -ac -screen scrn 1280x2000x24 :9.0 &
-echo "updateing"
+#echo "updateing"
 # 更新
-wget -O /app/resources/app.asar https://github.com/fuck-xuexiqiangguo/Fuck-XueXiQiangGuo/raw/master/app.asar
+#wget -O /app/resources/app.asar https://github.com/fuck-xuexiqiangguo/Fuck-XueXiQiangGuo/raw/master/app.asar
 
 # 运行程序
 echo "starting"
